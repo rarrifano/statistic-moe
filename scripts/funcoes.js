23a7b443e79f4c6018c00ad120c6f27c766fd17c
DougFanta <douglassoares.silva47@gmail.com>
2020-05-11 14:14:14 -0300

    Feita a implementação do gráfico

--- scripts/funcoes.js
+++ scripts/funcoes.js
@@ -1,13 +1,15 @@
 function trataInput(){
-    let valor = document.getElementById("input").value
+    let valor = document.getElementById("input2").value
+    if(valor == ''){
+        alert("[ERRO] Favor Informar os dados")
+    }else{    
     let sheetParamters = []
     sheetParamters.push(valor.split(';'))
-    let realSheetParameters = sheetParamters 
+    let realSheetParameters = sheetParamters
     realSheetParameters[0].sort((a,b) => a-b)
     console.log('vetor: ' + realSheetParameters[0])
     var countElements = {}
     realSheetParameters[0].forEach(function(i){ countElements[i] = (countElements[i]||0)+1})
-    console.log(countElements)
     let maiorNumero = realSheetParameters[0][realSheetParameters[0].length - 1]
     console.log("Maior: " + maiorNumero)
     let menorNumero = realSheetParameters[0][0]
@@ -15,7 +17,8 @@ function trataInput(){
     let totaldeIndicesVetor = realSheetParameters[0].length
     console.log("Total: " + totaldeIndicesVetor)
     return {realSheetParameters, maiorNumero, menorNumero, totaldeIndicesVetor, countElements}
-
+    }
+    
 }
 
 //Function what returns a number of lines and the number of colomns of a tabulation 
@@ -44,5 +47,56 @@ const quantitativaContinua = function(max, min, totalElem){
     }
         return formatoTabela
 }
-console.log(quantitativaContinua(56, 18, 37))
 
+
+function extraiobj(a){
+    this.frequencia = []
+    for(let i in a){
+        frequencia.push(a[i])
+        
+    }
+    return frequencia
+}
+
+
+
+function criaGrafico(){
+    let ctx = document.getElementById('grafico').getContext('2d')
+    var grafico = new Chart(ctx,{
+        type: 'bar',
+    data: {
+        labels: Object.keys(trataInput().countElements),
+        datasets: [{
+            label: document.getElementById('input').value,
+            data: extraiobj(trataInput().countElements) ,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(255, 206, 86, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(153, 102, 255, 0.8)',
+                'rgba(255, 159, 64, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
+    document.getElementById('input2').value = ''
+}
