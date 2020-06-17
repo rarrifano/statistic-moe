2dbcc6f4b365ed075932679a7e356855c416a262
Daniel Bispo <danieubispo@gmail.com>
2020-06-16 22:01:27 -0300

    Inclusao bootstrap

---
+++ bootstrap/assets/js/teste.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+//For Card Number formatted input
+var cardNum = document.getElementById('cr_no');
+cardNum.onkeyup = function (e) {
+if (this.value == this.lastValue) return;
+var caretPosition = this.selectionStart;
+var sanitizedValue = this.value.replace(/[^0-9]/gi, '');
+var parts = [];
+
+for (var i = 0, len = sanitizedValue.length; i < len; i +=4) { parts.push(sanitizedValue.substring(i, i + 4)); } for (var i=caretPosition - 1; i>= 0; i--) {
+    var c = this.value[i];
+    if (c < '0' || c> '9') {
+        caretPosition--;
+        }
+        }
+        caretPosition += Math.floor(caretPosition / 4);
+
+        this.value = this.lastValue = parts.join('-');
+        this.selectionStart = this.selectionEnd = caretPosition;
+        }
+
+        //For Date formatted input
+        var expDate = document.getElementById('exp');
+        expDate.onkeyup = function (e) {
+        if (this.value == this.lastValue) return;
+        var caretPosition = this.selectionStart;
+        var sanitizedValue = this.value.replace(/[^0-9]/gi, '');
+        var parts = [];
+
+        for (var i = 0, len = sanitizedValue.length; i < len; i +=2) { parts.push(sanitizedValue.substring(i, i + 2)); } for (var i=caretPosition - 1; i>= 0; i--) {
+            var c = this.value[i];
+            if (c < '0' || c> '9') {
+                caretPosition--;
+                }
+                }
+                caretPosition += Math.floor(caretPosition / 2);
+
+                this.value = this.lastValue = parts.join('/');
+                this.selectionStart = this.selectionEnd = caretPosition;
+                }
+
+                // Radio button
+                $('.radio-group .radio').click(function(){
+                $(this).parent().parent().find('.radio').removeClass('selected');
+                $(this).addClass('selected');
+                });
+                })
