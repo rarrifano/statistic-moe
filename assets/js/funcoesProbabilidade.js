d5710bb30dc122aba6e35d15b55900ccfc0d2159
Douglas <douglassoares.silva47@gmail.com>
2020-06-20 18:04:37 -0300

    implementação da probabilidade Binomial

--- assets/js/funcoesProbabilidade.js
+++ assets/js/funcoesProbabilidade.js
@@ -8,12 +8,27 @@ function intervalo(x) {
     section = document.querySelector("#escolhaintervalo")
 
     if (valor == "menor-que") {
-        section.innerHTML = `<input type="text" placeholder="Menor Que" class="form-control" id="menor">`
+        section.innerHTML = `<input type="number" placeholder="Menor Que" class="form-control" id="menor">`
     }
     if (valor == "entre") {
-        section.innerHTML = `<input type="text" placeholder="Entre" class="form-control" id="entreMenor"> E <input type="text " placeholder="Entre" class="form-control" id="entreMaior">`
+        section.innerHTML = `<input type="number" placeholder="Entre" class="form-control" id="entreMenor"> E <input type="number " placeholder="Entre" class="form-control" id="entreMaior">`
     }
     if (valor == "maior-que") {
-        section.innerHTML = `<input type="text" placeholder="Maior Que" class="form-control" id="maior">`
+        section.innerHTML = `<input type="number" placeholder="Maior Que" class="form-control" id="maior">`
+    }
+}
+
+function intervalo2(x) {
+    valor = x.value
+    section = document.querySelector("#escolhaintervaloNormal")
+
+    if (valor == "menor-que") {
+        section.innerHTML = `<input type="number" placeholder="Menor Que" class="form-control" id="menor">`
+    }
+    if (valor == "entre") {
+        section.innerHTML = `<input type="number" placeholder="Entre" class="form-control" id="entreMenor"> E <input type="number " placeholder="Entre" class="form-control" id="entreMaior">`
+    }
+    if (valor == "maior-que") {
+        section.innerHTML = `<input type="number" placeholder="Maior Que" class="form-control" id="maior">`
     }
 }
